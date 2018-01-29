6069058a835ae2f80dd2940aa366f4a26af67593
Magnalux <chaoberry@gmail.com>
2018-01-29 17:32:19 -0500

    Test code for passing a string of one number to the add function

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -6,6 +6,7 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
+
         return 1;
     }
 }
--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -7,8 +7,16 @@ public class StringCalculatorTest {
     StringCalculator stringCalculator = new StringCalculator();
     @org.junit.Test
     public void testAdd() throws Exception {
+        //Null string
         String testString = "";
         assertEquals(0, stringCalculator.add(testString));
 
+        //Sum of one number
+        testString = "0";
+        assertEquals(0, stringCalculator.add(testString));
+        testString = "9";
+        assertEquals(9, stringCalculator.add(testString));
+        testString = "5";
+        assertEquals(5, stringCalculator.add(testString));
     }
 }
